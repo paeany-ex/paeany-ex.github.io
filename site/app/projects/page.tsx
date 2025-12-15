18b3b188a79d958d4e71d3c3b156e877c46f7806
paeany-ex <paeany-ex@users.noreply.github.com>
2025-12-15 20:39:31 +0000

    deploy: 8c59b82a8a596daa96e26ed252b31e0593e80ab0

--- site/app/projects/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Projects() {
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Projects</h1>
-      <p>Coming soon...</p>
-    </div>
-  );
-}

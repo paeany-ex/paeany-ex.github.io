24d292b48a7736757fafd6741bdd4fa682a2aca8
paeany-ex <c1dr3flux@proton.me>
2025-12-15 13:29:58 -0500

    Update

--- site/app/archives/page.tsx
+++ site/app/archives/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from 'next/link';
 import fs from 'fs/promises';
 import path from 'path';
@@ -35,17 +34,20 @@ async function getPosts(): Promise<Post[]> {
   return posts.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 }
 
-export default async function Home() {
+export default async function Archives() {
   const posts = await getPosts();
 
   return (
     <div className="container mx-auto">
       <nav className="flex items-center justify-between p-4">
-        <div className="text-lg font-semibold">
+        <div className="text-lg font-semibold flex flex-col">
           <Link href="/">Archives</Link>
-          <div className="flex">
-            <Link href="/about" className="text-sm text-gray-500 hover:text-gray-600 duration-200">by Paeany</Link>
-          </div>
+          <Link href="/about" className="text-sm text-gray-500 hover:text-gray-600 duration-200">by Paeany</Link>
+        </div>
+        {/* Only show on small screens*/}
+        <div className="md:hidden flex space-x-4 mt-2">
+          <Link href="/archives" className="hover:text-gray-600 duration-400">Archives</Link>
+          <Link href="/projects" className="hover:text-gray-800 duration-400 font-medium">Projects</Link>
         </div>
         <div className="hidden md:flex space-x-4">
           <Link href="/archives" className="hover:text-gray-600 duration-400">Archives</Link>
@@ -56,10 +58,7 @@ export default async function Home() {
       <hr />
 
       <main className="min-h-screen p-4">
-        <h1 className="text-lg font-bold mb-4 uppercase font-serif italic max-w-lg">
-          "The most beautiful thing we can experience is the mysterious." – Albert Einstein
-        </h1>
-
+        <h1 className="text-2xl font-bold mb-4">Archives</h1>
         <div className="space-y-4">
           {posts.map(post => (
             <div key={post.slug} className="border pb-4 mb-4 p-4">
@@ -67,12 +66,11 @@ export default async function Home() {
                 {post.title}
               </Link>
               <p className="text-sm text-gray-500">{new Date(post.date).toLocaleDateString()}</p>
-              <p className="mt-2 text-gray-700">{post.excerpt}</p>
+              <p className="mt-2">{post.excerpt}</p>
             </div>
           ))}
         </div>
       </main>
-
       <footer></footer>
     </div>
   );
